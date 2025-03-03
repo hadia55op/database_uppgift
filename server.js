cbc19b4609048787fd5cf4dafd13d5a960d11411
hadia55op <hadizaidkhan@gmail.com>
2025-03-03 12:20:53 +0100

    added changes

---
+++ server.js
@@ -0,0 +1,209 @@
+const Database = require('better-sqlite3');
+const db = new Database('webbutiken.db');
+db.pragma('foreign_keys = ON');
+
+
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const { getProductDetails,getProductById,getProductByName,getproductsByCategory, deleteProductById,updateProduct, getCustomer,updateCustomer, getCustomerOrder} = require('./utilities.js');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+//############# 1 ###############
+app.get('/products', (req, res) => {
+    res.json(getProductDetails())});
+        
+
+//############# 2 ###############
+
+app.get('/products/stats', (req, res) => {
+        res.json(getProductsStats())});
+
+//############# 3 ###############
+
+app.get('/products/search', (req, res) => {
+const searchTerm = req.query.name || '';
+const descriptionSearchTerm = req.query.description || '';
+ try {
+        const products = getProductByName(searchTerm,descriptionSearchTerm);// utökad sök funktion flera fält samtidigt
+                 
+            if (products.length > 0) {
+                res.json(products);
+                 } else {
+                     
+                     res.status(404).json({ message: 'No products found matching the searchterm' });
+                 }
+             } catch (error) {
+                 console.error('Error fetching products:', error);
+                 res.status(500).json({ message: 'Internal Server Error' });  
+         }
+         })
+//############# 4 ###############
+
+app.get('/products/:id', (req, res) => {
+    res.send(getProductById(req.params.id))});
+
+
+//############# 5 ###############
+app.get('/products/category/:id', (req, res) => {
+    try {
+        const category = getproductsByCategory(req.params.id);
+        if (category) {
+            res.status(200).json(category); 
+        } else {
+            res.status(404).json({ message: 'category_id not found' }); 
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching category_id', error: error.message }); 
+    }
+});
+
+//############# 6 ###############
+
+app.post('/products', (req, res) => {
+    const { product_id,manufacturer_id, name, description, price, stock_quantity } = req.body;
+    // condition  check if any of the required fields are falsy.
+    if (![product_id, manufacturer_id, name, description, price, stock_quantity].every(Boolean)) {
+        return res.status(400).send('All keys and values are required');
+      }
+      // Insert new product into the products table
+    const stmt = db.prepare('INSERT INTO products (product_id,manufacturer_id, name, description, price, stock_quantity) VALUES (?, ?,?,?,?,?)');
+    const newProduct = stmt.run(product_id, manufacturer_id, name, description, price, stock_quantity);
+    const addedProduct = {
+        product_id: newProduct.lastInsertRowid, // SQLite provide this property to add the ID of the inserted product
+        manufacturer_id,
+        name,
+        description,
+        price,
+        stock_quantity
+      };
+    
+      res.status(201).json(addedProduct);
+    });
+    
+   
+//############# 7 ###############
+
+
+app.put('/products/:id', (req, res) => {
+    const { id } = req.params;
+    const { product_id, manufacturer_id, name, description, price, stock_quantity } = req.body;// required fields
+    if (!product_id && !manufacturer_id && !name && !description && !price && !stock_quantity) { 
+        return res.status(400).send("No fields to update.");
+    }
+    try {
+         if (price <= 50) {
+            throw new Error("Price must be greater than 50");// exrtra validation
+        }
+        const newProduct = updateProduct(id, manufacturer_id, name, description, price, stock_quantity, product_id);
+        
+        if (newProduct) {
+          res.json({ message: "Product updated successfully", product: newProduct });
+        }else {
+            res.status(404).send("Product not found.");
+        }
+    } catch (err) {
+        console.error("Error updating product:", err);
+        res.status(500).send("An error occurred while updating the product.");
+    }
+});
+
+//############# 8 ###############
+
+app.delete('/products/:id', (req, res) => {
+    res.send(deleteProductById(req.params.id));
+
+});
+//############# 9 ###############
+app.get(`/customers/:id`,(req, res) =>{
+    //res.send(getCustomer(req.params.id))});
+    try {
+        const customer = getCustomer(req.params.id);
+        if (customer.length > 0) {
+            res.json(customer);
+                 } else {
+                     
+                     res.status(404).json({ message: 'No customer for this id found ' });
+                 }
+             } catch (error) {
+                 console.error('Error fetching customers:', error);
+                 res.status(500).json({ message: 'Internal Server Error' });  
+         }
+    
+});
+//############# 10 ###############
+app.put('/customers/:Id', (req,res) =>{
+    const {Id}= req.params;
+     const {email, phone, address} = req.body
+     if(Id){
+        // Validate email format
+     const emailRegex = /\S+@\S+\.\S+/;
+    // Regex for email validation
+    if (email && !emailRegex.test(email)) {
+        return res.status(400).send("Invalid email format");
+    }
+        if(req.body.email){
+             Id.email = req.body.email;
+        }
+         if(req.body.phone){
+            Id.phone = req.body.phone;
+        }
+        if(req.body.address){
+            Id.address = req.body.address;
+         }
+    } else{
+         res.status(404).send("customer not found");
+     }
+    
+    
+
+    res.json(updateCustomer(email,phone,address,Id))
+    return updateCustomer(email,phone,address,Id);
+} );
+
+//############# 11 ###############
+
+app.get('/customers/:id/orders',(req, res) =>{
+    const customer_id = req.params.id;
+    try {
+        const customer = getCustomerOrder(customer_id);
+        if (customer) {
+            res.status(200).json(customer); // Return the customer if found
+        } else {
+            res.status(404).json({ message: 'customer_id not found' }); // If no customeris found
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching customer_id', error: error.message }); // In case of any error
+    }
+});
+
+   
+//############# 12 ###############
+
+app.get('/reviews/stats', (req, res) => {
+    try {
+      // SQL query to get average grade per product
+      const query = `app
+        SELECT p.product_id AS product_id, p.name AS product_name, AVG(r.rating) AS average_grade
+        FROM reviews r
+        JOIN products p ON r.product_id = p.product_id
+        GROUP BY p. product_id;
+      `
+  
+      // Execute the query
+      const reviewStats = db.prepare(query).all();
+  
+      // Return the results as JSON
+      res.json(reviewStats);
+    } catch (err) {
+        console.error('Error fetching stats:', err);
+        res.status(500).json({ error: 'Failed to fetch review stats' });
+      }
+    });
+    
+ 
---
+++ utilities.js
@@ -0,0 +1,144 @@
+const Database = require('better-sqlite3');
+
+
+const db = new Database('webbutiken.db');
+//############# 1 ###############
+function getProductDetails() {
+    try {
+      // SQL query with JOIN to include manufacturer and category info
+      const query = ` SELECT 
+    p.product_id AS product_id, 
+    p.name AS product_name,
+     ROUND(p.price, 2) AS product_price,
+    p.description,
+    c.name AS category_name,
+    m.name AS manufacturer_name 
+    FROM products p
+    JOIN manufacturers m ON p.manufacturer_id = m.manufacturer_id
+    JOIN products_categories pc ON p.product_id = pc.product_id
+    JOIN categories c ON pc.category_id = c.category_id;
+        
+      `;
+      const products = db.prepare(query).all(); // Execute the query
+      return products; // Return the result
+    }catch (err){
+        console.error('failed to gett all the products', err);
+    }
+  }
+ //############# 2 ###############
+function getProductsStats() {
+    try {
+            const resultStats = ( `  SELECT c.name AS category_name,
+         COUNT(DISTINCT p.product_id) AS total_products,
+         AVG(p.price) AS average_price
+FROM categories c
+LEFT JOIN products_categories pc ON c.category_id = pc.category_id
+LEFT JOIN products p ON pc.product_id = p.product_id
+GROUP BY c.category_id;
+
+            `)
+            const productStats = db.prepare(resultStats).all();
+            // Send the result as JSON response
+            res.json(productStats);
+        } catch (error) {
+         res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+      
+//############# 3 ###############
+function getProductByName(searchTerm) {
+    try {
+        const stmt = db.prepare('SELECT * FROM products WHERE LOWER(name) LIKE LOWER(?)  OR LOWER(description) LIKE LOWER(?)');
+        const products = stmt.all(`${searchTerm}`, `${searchTerm}`);
+        return products;
+    } catch (error) {
+
+        throw new Error('Error searching for products');
+    }
+}
+//############# 4 ###############
+function getProductById(product_id) {
+    try{const stmt = db.prepare('SELECT * FROM products WHERE product_id = ?');
+    return stmt.get(product_id);
+    }
+ catch (error) {
+
+    throw new Error('Error searching for products');
+
+}
+}
+//############# 5 ###############
+function getproductsByCategory(category_id) {
+    const stmt = db.prepare(`
+        SELECT p.product_id ,
+        p.name ,
+        p.price,
+        c.name AS category_name
+    FROM products p
+    INNER JOIN products_categories pc ON p.product_id = pc.product_id
+    INNER JOIN categories c ON c.category_id = pc.category_id
+        
+        
+    WHERE pc.category_id = ?
+
+    `);
+    
+    // we Use .all() to get all matching products
+    return stmt.all(category_id);
+}
+//############# 6 in  server.js ###############
+
+//############# 7 ###############
+function updateProduct(product_id, manufacturer_id, name, description, price, stock_quantity, new_id) {
+    try {
+       
+        //  Update the product with the new values, setting product_id to the new_id
+        const stmt = db.prepare('UPDATE products SET product_id = ?, manufacturer_id = ?, name = ?, description = ?, price = ?, stock_quantity = ? WHERE product_id = ?');
+        
+        return stmt.run(new_id, manufacturer_id, name, description, price, stock_quantity, product_id);
+    } catch (err) {
+        console.error('Failed to update product: ', err);
+    }
+}
+//############# 8 ###############
+function deleteProductById(product_id){
+    const stmt = db.prepare('DELETE FROM products WHERE product_id = ?');
+    return stmt.run(product_id);
+}
+
+
+//############# 9 ###############
+  function getCustomer(customer_id){
+    const stmt = db.prepare(`SELECT 
+   * FROM customers AS c
+INNER JOIN orders AS o ON o.customer_id = c.customer_id
+WHERE c.customer_id = ?
+  `)
+        return stmt.all(customer_id);
+}
+//############# 10 ###############
+function updateCustomer(email, phone,address, customer_id){
+    try{
+       
+        const stmt = db.prepare('UPDATE customers SET email = ?, phone = ? , address = ? WHERE customer_id = ?');
+        return stmt.run(email, phone, address, customer_id);
+        
+
+    } catch(err){
+        console.error('failed to update customer: ', err);
+    }
+
+}
+//############# 11 ###############
+
+function getCustomerOrder(customer_id){
+    const stmt = db.prepare(`SELECT * FROM orders WHERE customer_id = ?`)
+    return stmt.all(customer_id);
+}
+
+//############# 12 in server.js ###############
+
+
+
+
+module.exports = {getProductDetails,getProductsStats,getProductById, getProductByName,getproductsByCategory,deleteProductById, updateProduct,getCustomer, updateCustomer, getCustomerOrder };
